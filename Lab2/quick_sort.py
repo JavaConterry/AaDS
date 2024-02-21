1de21c416661541a317e6bdf7fda6b87c48f326a
JavaConterry <conterry05@gmail.com>
2024-02-21 16:56:45 +0100

    Lab2: minor fixes

--- Lab2/quick_sort.py
+++ Lab2/quick_sort.py
@@ -5,11 +5,11 @@ def quick_sort(arr, return_complexcity = False):
     p = arr[-1] # pivot set to be the end value
     i, j = -1, 0 # initial indecies
     while j<len(arr)-1:
+        comparisons += 1
         if(arr[j]<p):
-            comparisons += 1
             i+=1
+        comparisons += 1
         if(i!=-1 and arr[i]>arr[j]): # swap if two are descending
-            comparisons += 1
             swaps += 1
             # print('a')
             res = arr[i] 
@@ -17,8 +17,8 @@ def quick_sort(arr, return_complexcity = False):
             arr[j] = res
         j+=1
     i+=1
+    comparisons += 1
     if(arr[i]>p): # final swap if end value i and pivot are descending
-        comparisons += 1
         swaps += 1
         res = arr[i]
         arr[i] = p 
